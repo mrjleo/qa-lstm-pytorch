79ed8c9bfb295c8fad97e853bc38dab8641bd267
mrjleo <mrjleo@users.noreply.github.com>
2020-05-24 18:39:58 +0200

    train on all pairs

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from data_source import TrainDataset
 
 from qa_utils.misc import Logger
 from qa_utils.io import get_cuda_device
-from qa_utils.training import save_args
+from qa_utils.training import prepare_logging
 
 
 def loss(s_pos, s_neg, margin):
@@ -39,12 +39,7 @@ def main():
 
     torch.manual_seed(args.random_seed)
     device = get_cuda_device()
-    ckpt_dir = os.path.join(args.working_dir, 'ckpt')
-    os.makedirs(ckpt_dir, exist_ok=True)
-
-    # save all args in a file
-    args_file = os.path.join(args.working_dir, 'args.csv')
-    save_args(args_file, args)
+    logger, ckpt_dir = prepare_logging(args)
 
     train_file = os.path.join(args.DATA_DIR, 'train.h5')
     train_ds = TrainDataset(train_file)
@@ -57,32 +52,27 @@ def main():
     model = torch.nn.DataParallel(model)
 
     optimizer = torch.optim.Adam(model.parameters())
-    log_file = os.path.join(args.working_dir, 'train.csv')
-    logger = Logger(log_file, ['epoch', 'loss'])
     model.train()
     for epoch in range(args.epochs):
         epoch_losses = []
         for batch in tqdm(train_dl, desc='epoch {}'.format(epoch)):
             model.zero_grad()
             batch_losses = []
-            pos_sims, neg_sims = model(*batch)
-            for pos_sim, neg_sims_item in zip(pos_sims, neg_sims):
+
+            pos_sims, all_neg_sims = model(*batch)
+            for pos_sim, neg_sims in zip(pos_sims, all_neg_sims):
                 # pos_sim is a single number with shape ()
                 # add a dimension and expand the shape to (num_neg_examples,)
-                pos_sim = pos_sim.unsqueeze(0).expand(train_ds.num_neg_examples)
-                losses = loss(pos_sim, neg_sims_item, args.margin)
+                pos_sims = pos_sim.unsqueeze(0).expand(train_ds.num_neg_examples)
+                # we get better results training on all pairs instead of just the one with max. loss
+                batch_losses.extend(loss(pos_sims, neg_sims, args.margin))
 
-                # take just the example with maximum loss as per the paper
-                max_loss = max(losses)
-                batch_losses.append(max_loss)
-                epoch_losses.append(max_loss.cpu().detach().numpy())
             batch_loss = torch.mean(torch.stack(batch_losses, 0).squeeze(), 0)
-            batch_loss.requires_grad_()
+            epoch_losses.append(batch_loss.cpu().detach().numpy())
             batch_loss.backward()
             optimizer.step()
 
         logger.log([epoch, np.mean(epoch_losses)])
-
         # save the module state dict, since we use DataParallel
         state = {'epoch': epoch, 'state_dict': model.module.state_dict(),
                  'optimizer': optimizer.state_dict()}
